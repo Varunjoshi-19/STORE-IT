539b1b2944221fbecfa4faf9e8bd8239de553ddd
Varunjoshi-19 <varunjoshi6283@gmail.com>
2025-06-23 22:53:00 +0530

    fix deployment

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import MobileNavigation from "@/components/MobileNavigation"
 import Sidebar from "@/components/Sidebar"
 import { getCurrentUser } from "@/lib/actions/user.actions"
 import { Toaster } from "@/components/ui/toaster"
+import { redirect } from "next/navigation"
 
 export const dynamic = 'force-dynamic';
 
@@ -10,7 +11,7 @@ async function layout({ children }: { children: React.ReactNode }) {
 
   const currentUser = await getCurrentUser();
 
-  //  if(!currentUser) return redirect("/sign-in");
+   if(!currentUser) return redirect("/sign-in");
 
   return (
     <main className="flex h-screen">
